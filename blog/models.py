52bf8b0b9d05bf52d425a8a514d997716bd413e0
mikesealey <mikesealey@hotmail.com>
2025-03-09 09:04:44 +0000

    adds comments

--- blog/models.py
+++ blog/models.py
@@ -13,3 +13,16 @@ class Post(models.Model):
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
+    excerpt = models.TextField(blank=True)
+    updated_on = models.DateField(auto_now = True)
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="comments"
+    )
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="comments"
+    )
+    body = models.TextField()
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
