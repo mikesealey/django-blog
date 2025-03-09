52d53c5b1ce55bdf4940d761830e25597049fcd0
mikesealey <mikesealey@hotmail.com>
2025-03-09 16:27:04 +0000

    prepares for adding frontened

--- blog/models.py
+++ blog/models.py
@@ -15,6 +15,16 @@ class Post(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
     updated_on = models.DateField(auto_now = True)
+    field_2 = models.CharField(default="Hellow WOrld")
+    field_3 = models.CharField(null=True)
+
+    def __str__(self):
+        return f"{self.title} | written by {self.author}"
+    
+
+
+    class Meta:
+        ordering = ["-created_on"]
 
 class Comment(models.Model):
     post = models.ForeignKey(
@@ -26,3 +36,10 @@ class Comment(models.Model):
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"{self.body} by {self.author}"
+
+
