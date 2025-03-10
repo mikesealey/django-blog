e4b84af7aad869d6d7c9349df082db649523c182
mikesealey <mikesealey@hotmail.com>
2025-03-10 09:54:42 +0000

    wip

--- blog/models.py
+++ blog/models.py
@@ -15,8 +15,6 @@ class Post(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
     updated_on = models.DateField(auto_now = True)
-    field_2 = models.CharField(default="Hellow WOrld")
-    field_3 = models.CharField(null=True)
 
     def __str__(self):
         return f"{self.title} | written by {self.author}"
