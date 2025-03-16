8f57784e50fd31ff25a21f09caae9c4da3bc36b1
mikesealey <mikesealey@hotmail.com>
2025-03-16 21:30:32 +0000

    allows users to edit comments

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,9 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic
 from django.contrib import messages
-from .models import Post
+from django.http import HttpResponseRedirect
+from .models import Post, Comment
 from .forms import CommentForm
-
-
 
 # Create your views here.
 class PostList(generic.ListView):
@@ -42,6 +41,14 @@ def post_detail(request, slug):
                 request, messages.SUCCESS,
                 'Comment submitted and awaiting approval'
             )
+    
+    if request.method == "POST":
+        print("Received a POST request")
+
+    comment_form = CommentForm()
+    print("About to render template")
+
+    
 
     return render(
         request,
@@ -53,3 +60,25 @@ def post_detail(request, slug):
         "comment_form": comment_form,
         },
     )
+
+def comment_edit(request, slug, comment_id):
+    """
+    view to edit comments
+    """
+    if request.method == "POST":
+
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comment = get_object_or_404(Comment, pk=comment_id)
+        comment_form = CommentForm(data=request.POST, instance=comment)
+
+        if comment_form.is_valid() and comment.author == request.user:
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.approved = False
+            comment.save()
+            messages.add_message(request, messages.SUCCESS, 'Comment Updated!')
+        else:
+            messages.add_message(request, messages.ERROR, 'Error updating comment!')
+
+    return HttpResponseRedirect(reverse('post_detail', args=[slug]))
